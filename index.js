f9d82a338e49647d02314ee8fa56a7728a4af90b
Athagod <afordhal10@mail.bw.edu>
2020-04-25 11:17:20 -0400

    add employee

--- index.js
+++ index.js
@@ -10,51 +10,51 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: "",
-    database: "employee_db" 
+    database: "employee_db"
 });
 
 
 // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourRootPassword
 
-connection.connect((err) =>{
+connection.connect((err) => {
     if (err) {
         throw err
-    } 
+    }
 
     askQuestion()
 
 });
 
-function  askQuestion () {
+function askQuestion() {
     inquirer
         .prompt({
             name: "action",
-            type: "list", 
-            message: "Choose from the following", 
+            type: "list",
+            message: "Choose from the following",
             options: [
                 "view employees",
                 "view departments",
-                "view roles", 
+                "view roles",
                 "add employee",
-                "delete employee",                   
+                "delete employee",
             ]
 
         })
         .then((answer) => {
             switch (answer.action) {
-               
+
                 case "view employee":
                     viewEmployee();
                     break;
-    
+
                 case "view departemnt":
                     viewDepartment();
                     break;
-    
+
                 case "view role":
                     viewRole();
                     break;
-               
+
                 case "add employee":
                     addEmployee();
                     break;
@@ -69,7 +69,7 @@ function  askQuestion () {
 
 
 function viewEmployee() {
-    connection.query("select * FROM employee", function(err, res){
+    connection.query("SELECT * FROM employee", function (err, res) {
         if (err) throw err
         console.log(res);
 
@@ -78,38 +78,66 @@ function viewEmployee() {
 
 
 function viewDepartment() {
-    connection.query("select * from department", function (err,res){
-        let departmentOptions = res.map(function (department){
-            return {name: department.name, value: department.id}
+    connection.query("SELECT * FROM department", function (err, res) {
+        let departmentOptions = res.map(function (department) {
+            return { name: department.name, value: department.id }
         })
         inquirer
-            .prompt([
+            .prompt(
                 {
                     type: "list",
                     name: "departmentOptions",
-                    message: "Choose Department", 
+                    message: "Choose Department",
                     choices: departmentOptions
                 })
-                .then(function(userChoise) {
-
-                })
-                {
-                    name: "lastName",
-                    type: "input",
-                    message: "Enter employee's last name"
-                },
-                {
-                    name: "employeeRole",
-                    tyoe: "list", 
-                    message: "Enter role", 
-                }
-            ])
-            // .then((firstNmae, lastName, employeeRole))
+            .then(function (userChoise) {
+                connection.query(`select first_name, last_name from employee where role_id=${userChoise.departmentOptions}`,
+                    function (err, res) {
+                        console.log(res);
+                        askQuestion()
+                    }
+                )
+            })
     })
-}
+};
+
+function addEmployee() {
+    inquirer
+        .prompt ([
+            {
+                type: "input",
+                name: "addEmployee",
+                message: "Enter employee's first name",
+                answer: ""
+            },
+            {
+                type: "input",
+                name: "addEmployee",
+                message: "Enter employee's last name",
+                answer: ""
+            },
+            {
+                type: "list",
+                name: "addEmployee",
+                message: "Choose Employee's Role",
+                choices: [
+                    "Electrictrician",
+                    "Finanical Adviser",
+                    "Loan Officer",
+                    "Software Engineer",
+                    "Sales Rep"
+                ]
+            },
+            
+            }
+        ])
+} 
 
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on: http://localhost${PORT}`);
-});
+
+
+
+    app.listen(PORT, () => {
+        console.log(`Server is listening on: http://localhost${PORT}`);
+    });
 
